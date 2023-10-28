fa6f9a47fa87c32187b3da7d33e1419028eb7ee5
huanghao <1063462242@qq.com>
2023-10-28 14:19:44 +0800

    transition 自定义类名，结合第三方动画库来animate.css使用

--- vue3-base/src/views/transition.vue
+++ vue3-base/src/views/transition.vue
@@ -2,15 +2,35 @@
    <div class="content">
       <button @click="flag = !flag">switch</button>
       <!-- 用法1  name属性定义class-->
-      <transition name="fade">
+      <!-- <transition name="fade">
+        <div v-if="flag" class="box"></div>
+      </transition> -->
+      <!-- 用法2  自定义类名 -->
+      <!-- <transition 
+        enter-from-class="aa-enter-from"
+        enter-active-class="aa-enter-active"
+        enter-to-class="aa-enter-to"
+        leave-from-class="aa-leave-from"
+        leave-active-class="aa-leave-active"
+        leave-to-class="aa-leave-to">
+        <div v-if="flag" class="box"></div>
+    </transition> -->
+    <!-- 结合第三方类库 animate.css 来使用 -->
+    <!-- duration: 动画时长,值有一个值或对象：number｜{enter:number, leave:number} -->
+      <transition 
+       :duration="1000"
+        enter-active-class="animate__animated animate__flipInX"
+        leave-active-class="animate__animated animate__fadeOutDown">
         <div v-if="flag" class="box"></div>
       </transition>
+
    </div>
  <div></div>
 
 </template>
 <script setup lang='ts'>
 import{ref} from 'vue'
+import 'animate.css'
 const flag = ref<boolean>(true)
 
 </script>
@@ -44,6 +64,34 @@ const flag = ref<boolean>(true)
     transition: all 1.5s ease;
 }
 .fade-leave-to { // 离开 到 XX 结束
+    width: 0;
+    height: 0;
+}
+
+
+// 用法2 自定义类名
+.aa-enter-from {
+    width: 0;
+    height: 0;
+}
+.aa-enter-active {
+    // 过程
+    transition: all 1.5s ease;
+}
+.aa-enter-to { // 进入 到 XX 结束
+    width: 200px;
+    height: 200px;
+    transform: scale(2); // 放大2倍
+}
+.aa-leave-from {
+    width: 200px;
+    height: 200px;
+}
+.aa-leave-active {
+    // 过程
+    transition: all 1.5s ease;
+}
+.aa-leave-to { // 离开 到 XX 结束
     width: 0;
     height: 0;
 }
